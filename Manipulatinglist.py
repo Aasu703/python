1435f0a872db9ae1cc6dadd13a0a5cc1b952b9ba
Aasu703 <smellylips508@gmail.com>
2025-11-21 14:25:22 +0545

    Manipulation of string

---
+++ Manipulatinglist.py
@@ -0,0 +1,54 @@
+# # list manipulations
+# # change list elements
+# # Add list elements 
+
+
+# fam = ["liz", 1.73, "emma", 1.68, "mom", 1.65, "dad", 1.80]
+# fam[7] = 1.86
+
+# # addiing and removing elements
+
+# fam + ["me", 1.76]
+
+# print(fam)
+
+# # del
+# del fam[2]
+# print(fam)
+
+# x =      ["a", "b", "c"]
+# #index     0    1    2 
+# #negative -3   -2   -1
+# y=x
+# y[1] = "z"
+# print(y)
+
+
+# x = ["a", "b", "c"]
+# y = list(x)  # creates a copy of list x
+# y=x[:]
+
+# print(y)
+
+
+
+# Create the areas list
+# areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# # Correct the bathroom area
+# areas[-1] = 10.50
+# # Change "living room" to "chill zone"
+# areas[4] = "chill zone"
+
+# # print(areas)
+
+# area_1 = areas + ["poolhouse", 24.5, "garage", 15.45]
+# # print(area_1)
+
+
+# del area_1[10:12]
+# print(area_1)
+
+x = [0,1,-2]
+x[1:] = [3,4,5]
+print(x)
--- Manipulatinglists.py
+++
@@ -1,9 +0,0 @@
-# list manipulations
-# change list elements
-# Add list elements 
-
-
-fam = ["liz", 1.73, "emma", 1.68, "mom", 1.65, "dad", 1.80]
-fam[7] = 1.86
-print(fam)
-
