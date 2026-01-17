71b2b7f421c209c4686bee666c50fe181ce5e02f
Aasu703 <smellylips508@gmail.com>
2026-01-17 13:19:24 +0545

    working with instance

--- oop/instance.py
+++ oop/instance.py
@@ -6,10 +6,18 @@ class Book:
         self.author = author
         self.pages = pages
         self.price = price
+        self.__secret = "Secret"
 
     # TODO: create instance methods
     def getprice(self):
-        return self.price
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+
+            return self.price
+
+    def setdiscount(self, amount):
+        self._discount = amount # _discount is a protected property or known as private property
 
 # TODO : create instance of the class
 book1 = Book("SO meow","MU_KC", 9866, 1000)
@@ -18,3 +26,8 @@ book2 = Book("meow pheow","I_AM_KC", 321, 321)
 # TODO : print the class and property
 print(book1)
 print(book1.title)
+print(book2.getprice())
+book2.setdiscount(0.25)
+print(book2.getprice())
+
+print(book2.__secret)  # This will raise an AttributeError because __secret is a private property
